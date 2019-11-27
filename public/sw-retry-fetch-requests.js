1db0d73bc5a8b20d778805c14e283b83e5a87041
Jess Chadwick <jess.chadwick@centralreach.com>
2019-11-27 11:26:57 -0500

    Adding service worker pub-sub

--- public/sw-retry-fetch-requests.js
+++ public/sw-retry-fetch-requests.js
@@ -1,9 +1,29 @@
 /* eslint-disable */
 console.info("Loaded service worker!");
 
+const publishMessage = async message => {
+  const clients = await this.clients.matchAll();
+  clients.forEach(client => client.postMessage(message));
+};
+
+const updateOnlineStatus = () =>
+  publishMessage({ type: "online_status", isOnline: navigator.onLine });
+
+(function listenForOffline() {
+  addEventListener("online", updateOnlineStatus);
+  addEventListener("offline", updateOnlineStatus);
+})();
+
 const DefaultRetryCount = 5;
 
-function retryDelay(attempt) {
+function DefaultRetryDelay(attempt) {
+  if (!navigator.onLine) {
+    console.debug(
+      `No network connection detected -- waiting an hour to try again`
+    );
+    return 60 * 60 * 1000; // 1 hr
+  }
+
   const delay = attempt == 1 ? 0 : Math.pow(2, attempt) * 500;
   console.debug(`Retry Delay: ${delay}`);
   return delay;
@@ -11,14 +31,26 @@ function retryDelay(attempt) {
 
 addEventListener("fetch", event => {
   const { request } = event;
-  const { retryCount } = request;
+  const { retryCount, retryDelay } = request;
 
   event.respondWith(
-    fetchWithRetry({ request, retryCount: retryCount || DefaultRetryCount })
+    fetchWithRetry({
+      request,
+      retryCount: retryCount || DefaultRetryCount,
+      retryDelay: retryDelay || DefaultRetryDelay
+    })
   );
 });
 
-function fetchWithRetry({ request, url, retryAttempt = 1, retryCount }) {
+function fetchWithRetry({
+  request,
+  retryAttempt = 1,
+  retryCount,
+  retryDelay,
+  url
+}) {
+  publishMessage("[POSTMESSAGE] Retrying!");
+
   return new Promise((resolve, reject) => {
     console.debug("Executing with retry");
 
@@ -41,7 +73,8 @@ function fetchWithRetry({ request, url, retryAttempt = 1, retryCount }) {
             url: request.url,
             request,
             retryAttempt: retryAttempt + 1,
-            retryCount
+            retryCount,
+            retryDelay
           })
             .then(resolve)
             .catch(reject),
